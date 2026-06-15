0633fd6fd32e9790fcc0210fc2062ca53ccda74a
Haejun-Lee <haejun2308@gmail.com>
2026-06-15 16:35:12 +0900

    [Fix] LHJ Title 수정

--- UNSEEN/Assets/Scripts/Title/TitleManager.cs
+++ UNSEEN/Assets/Scripts/Title/TitleManager.cs
@@ -9,7 +9,6 @@ using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
-    public GameObject titleUI;
     public GameObject settingUI;
 
     public void OnPlayButton()
@@ -19,14 +18,12 @@ public class TitleManager : MonoBehaviour
 
     public void OnSettingButton()
     {
-        titleUI.SetActive(false);
         settingUI.SetActive(true);
     }
 
     public void OnCloseSettingButton()
     {
         settingUI.SetActive(false);
-        titleUI.SetActive(true);
     }
 
     public void OnExitButton()
